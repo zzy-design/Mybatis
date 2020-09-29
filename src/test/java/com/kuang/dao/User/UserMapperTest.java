9cf3c8f21560a744f02e5bd462b5094a600671dc
zzy-design <69064727+zzy-design@users.noreply.github.com>
2020-09-29 14:49:04 +0800

    练习

--- src/test/java/com/kuang/dao/User/UserMapperTest.java
+++ src/test/java/com/kuang/dao/User/UserMapperTest.java
@@ -7,6 +7,7 @@ import org.apache.ibatis.session.SqlSession;
 import org.apache.log4j.Logger;
 import org.junit.Test;
 import redis.clients.jedis.Jedis;
+import redis.clients.jedis.Protocol;
 
 import java.util.*;
 
@@ -199,8 +200,16 @@ public class UserMapperTest {
         System.out.println("服务正在运行: " + jedis.ping());
         //设置 redis 字符串数据
         jedis.set("runoobkey", "www.runoob.com");
-        String r = jedis.get("r");
         // 获取存储的数据并输出
-        System.out.println("redis 存储的字符串为: "+ jedis.get("runoobkey"));
+        System.out.println("redis 存储的字符串为: " + jedis.get("runoobkey"));
+        System.out.println("------------");
+        jedis.lpush("site-list", "Runoob");
+        jedis.lpush("site-list", "Google");
+        jedis.lpush("site-list", "Taobao");
+        //获取存储的数据并输出
+        List<String> list = jedis.lrange("site-list", 0, 2);
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println("列表项为: " + list.get(i));
+        }
     }
 }
